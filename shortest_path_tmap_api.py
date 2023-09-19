5e6aaaa00017ab1a6f97dd73a9870792fead0b60
minsoong2 <happyms817@gmail.com>
2023-09-19 14:27:00 +0900

    230919

--- shortest_path_tmap_api.py
+++ shortest_path_tmap_api.py
@@ -18,7 +18,15 @@ folium.Marker([stopover_lat, stopover_lng], tooltip='경유지').add_to(m)
 tmap_api_key = 'QaALuzWbgm8PXyhzGSGgM9HHtAdgGuGL7JWocrK9'
 
 # Tmap API 경로 요청 URL 생성
-url = f'https://apis.openapi.sk.com/tmap/routes/pedestrian?version=1&format=json&appKey={tmap_api_key}&startX={start_lng}&startY={start_lat}&startName=출발지&passList={stopover_lng},{stopover_lat}&endX={end_lng}&endY={end_lat}&endName=목적지&searchOption=4'
+url = f'https://apis.openapi.sk.com/tmap/routes/pedestrian?version=1&format=json&appKey={tmap_api_key}&startX={start_lng}&startY={start_lat}&startName=출발지&passList={stopover_lng},{stopover_lat}&endX={end_lng}&endY={end_lat}&endName=목적지&searchOption=0'
+# 경로탐색 시 우선순위 옵션 -> searchOption
+# - 00 : 교통최적+추천 (기본 값)
+# - 01 : 교통최적+무료우선
+# - 02 : 교통최적+최소시간
+# - 03 : 교통최적+초보
+# - 04 : 교통최적+고속도로우선
+# - 10 : 최단거리+유/무료
+# - 19 : 교통최적+어린이보호구역 회피
 
 response = requests.get(url)
 data = response.json()
@@ -29,7 +37,7 @@ for feature in data['features']:
     if feature['geometry']['type'] == 'LineString':
         coordinates.extend([(coord[1], coord[0]) for coord in feature['geometry']['coordinates']])
 
-
+print(coordinates)
 print(len(coordinates))
 polyline = folium.PolyLine(locations=coordinates, color='blue', weight=5).add_to(m)
 plugins.PolyLineTextPath(
