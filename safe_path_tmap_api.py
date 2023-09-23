969c659e2f56c5cec7769a3bbf123be418c9835d
minsoong2 <happyms817@gmail.com>
2023-09-23 18:26:00 +0900

    230923

--- safe_path_tmap_api.py
+++ safe_path_tmap_api.py
@@ -6,20 +6,18 @@ import pandas as pd
 
 
 def haversine(lat1, lon1, lat2, lon2):
-    # 지구의 반경 (지구의 반경은 평균 반경을 사용합니다)
-    radius = 6371  # 지구 반경 (단위: 킬로미터)
+    # 지구 radius
+    radius = 6371  # km
 
-    # 라디안으로 변환
     lat1 = math.radians(lat1)
     lon1 = math.radians(lon1)
     lat2 = math.radians(lat2)
     lon2 = math.radians(lon2)
 
-    # 위도와 경도의 차이 계산
     dlat = lat2 - lat1
     dlon = lon2 - lon1
 
-    # Haversine 공식을 사용하여 거리 계산
+    # Haversine 공식 활용
     a = math.sin(dlat/2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon/2)**2
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
     distance = radius * c
@@ -31,13 +29,10 @@ start_lat = 35.22241510564285
 start_lng = 128.6879361625245
 end_lat = 35.236619713404615
 end_lng = 128.69428289085678
-stopover_lat = 37.5675000  # 경유지 위도
-stopover_lng = 126.9790000  # 경유지 경도
 
 m = folium.Map(location=[start_lat, start_lng], zoom_start=15)
 folium.Marker([start_lat, start_lng], tooltip='출발지').add_to(m)
 folium.Marker([end_lat, end_lng], tooltip='목적지').add_to(m)
-folium.Marker([stopover_lat, stopover_lng], tooltip='경유지').add_to(m)
 
 # Tmap API 키
 tmap_api_key = 'QaALuzWbgm8PXyhzGSGgM9HHtAdgGuGL7JWocrK9'
@@ -45,17 +40,16 @@ tmap_api_key = 'QaALuzWbgm8PXyhzGSGgM9HHtAdgGuGL7JWocrK9'
 # Tmap API 경로 요청 URL 생성
 url = f'https://apis.openapi.sk.com/tmap/routes/pedestrian?version=1&format=json&appKey={tmap_api_key}&startX={start_lng}&startY={start_lat}&startName=출발지&endX={end_lng}&endY={end_lat}&endName=목적지&searchOption=0'
 # 경로탐색 시 우선순위 옵션 -> searchOption
-# - 00 : 교통최적+추천 (기본 값)
-# - 01 : 교통최적+무료우선
-# - 02 : 교통최적+최소시간
-# - 03 : 교통최적+초보
-# - 04 : 교통최적+고속도로우선
-# - 10 : 최단거리+유/무료
-# - 19 : 교통최적+어린이보호구역 회피
+# - 00 : 교통최적 + 추천 (기본 값)
+# - 01 : 교통최적 + 무료우선
+# - 02 : 교통최적 + 최소시간
+# - 03 : 교통최적 + 초보
+# - 04 : 교통최적 + 고속도로우선
+# - 10 : 최단거리 + 유/무료
+# - 19 : 교통최적 + 어린이보호구역 회피
 
 response = requests.get(url)
 data = response.json()
-print(data)
 
 coordinates = []
 for feature in data['features']:
@@ -94,6 +88,7 @@ for i in range(interval):
 cctv_data_file = r"C:\Users\minsoo\OneDrive - 창원대학교\바탕 화면\창원cctv_data.csv"
 cctv_df = pd.read_csv(cctv_data_file)
 cctv_locations = cctv_df[['WGS84위도', 'WGS84경도']]
+
 # CCTV 위치 - Marker
 for _, row in cctv_locations.iterrows():
     lat, lon = row['WGS84위도'], row['WGS84경도']
@@ -102,9 +97,10 @@ for _, row in cctv_locations.iterrows():
 police_data_file = r"C:\Users\minsoo\OneDrive - 창원대학교\바탕 화면\창원police_data.csv"
 police_df = pd.read_csv(police_data_file)
 police_locations = police_df[['위도', '경도']]
+
 # Police 위치 - Marker
 for _, row in police_locations.iterrows():
     lat, lon = row['위도'], row['경도']
-    folium.Marker([lat, lon], icon=folium.Icon(color='red'), tooltip='경찰서 위치').add_to(m)
+    folium.Marker([lat, lon], icon=folium.Icon(color='blue'), tooltip='경찰서 위치').add_to(m)
 
 m.save('safe_path_tmap.html')
